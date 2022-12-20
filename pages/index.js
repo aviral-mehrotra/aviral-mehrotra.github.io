9fc9290b9f16f65dbd7e3a65f73ca3d0113b40f8
theAvman <aviral.mehrotra@gmail.com>
2022-12-20 15:10:33 -0600

    fixing landing view

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
               <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="resume.pdf" download="Aviral Mehrotra's Resume">Resume</a></li>
             </ul>
           </nav>
-          <div className='relative w-20 h-20'>
+          <div className='relative w-5 h-5 md:w-20 md:h-20'>
             <Image />
           </div>
           <div className='text-center p-10'>
@@ -40,7 +40,8 @@ export default function Home() {
             </h3>
             <p className='text-md py-1 leading-8 text-gray-800 md:text-2xl max-w-lg mx-auto dark:text-slate-200'>
               High school student interested in computer science,
-              AI and machine learning, economics, and emerging technologies.</p>
+              AI and machine learning, economics, and emerging technologies.
+            </p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-200'>
             <a target="_blank" href="https://github.com/aviral-mehrotra" rel="noopener noreferrer"><AiFillGithub /></a>
